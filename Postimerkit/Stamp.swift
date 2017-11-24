49c8e78fd0f3df6d563029838fcb9aec488c9007
LassilaJoni <joni@jonilassila.com>
2017-11-24 11:17:36 +0200

    UITableView now has a datasource and it works.

--- Postimerkit/Stamp.swift
+++ Postimerkit/Stamp.swift
@@ -12,12 +12,27 @@ class Stamp: Codable {
     let keywords: String!
     let releaseDate: String!
     let usageEndDate: String?
-    let faceValue: String?
     let name: String?
     let color: String?
-    let printingPlace: String?
-    let printingQuantity: Int?
+    let printPlace: String?
+    let printQuantity: Int?
     let artist: String?
     let currency: String?
     let imageUrl: String?
+}
+
+extension Stamp {
+    class func getStamps(completion: @escaping (_ result: [Stamp]) -> Void) {
+        if let fileUrl = Bundle.main.url(forResource: "stamps", withExtension: "json") {
+            if let data = try? Data(contentsOf: fileUrl) {
+                let decoder = JSONDecoder()
+                do {
+                    let stamps = try decoder.decode([Stamp].self, from: data)
+                    completion(stamps)
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
 }
--- Postimerkit/ViewController.swift
+++ Postimerkit/ViewController.swift
@@ -8,20 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var stamps = [Stamp]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Stamp.getStamps(completion: { stamps in
+            self.stamps = stamps
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.stamps.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let stamp = self.stamps[indexPath.row]
+        
+        cell.textLabel?.text = stamp.name
+        cell.detailTextLabel?.text = stamp.artist
+        
+        return cell
+    }
 }
 
